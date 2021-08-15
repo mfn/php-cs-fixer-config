4aafd903e4d4fefa6d646af7bc9abfb8201c1df8
Markus Podar <markus@fischer.name>
2021-08-15 22:47:58 +0200

    Adapt name change of php-cs-fixer 2 to 3 upgrade

--- src/Config.php
+++ src/Config.php
@@ -276,7 +276,7 @@ class Config
             'ternary_operator_spaces' => true,
             'ternary_to_elvis_operator' => false,
             'ternary_to_null_coalescing' => true,
-            'trailing_comma_in_multiline_array' => true,
+            'trailing_comma_in_multiline' => true,
             'trim_array_spaces' => true,
             'unary_operator_spaces' => true,
             'use_arrow_functions' => false,
